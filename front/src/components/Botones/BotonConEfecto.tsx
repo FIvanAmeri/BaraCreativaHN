dfc2b04cb86f9f7cc2995b4f787742cdf8b28718
Ivan Ameri <dark.ranked@gmail.com>
2025-08-22 13:19:43 -0300

    Modificacion de boton Acceso

--- front/src/components/Botones/BotonConEfecto.tsx
+++ front/src/components/Botones/BotonConEfecto.tsx
@@ -19,23 +19,19 @@ export default function BotonConEfecto({
   const commonClasses = `
     flex items-center justify-center
     bg-red-600 text-white relative overflow-hidden group z-10
-    duration-1000 rounded-[1.25rem]  /* 20px */
-    h-auto py-2 sm:py-3
-    text-xs sm:text-sm md:text-base lg:text-xl
-    w-auto min-w-[6rem] max-w-[12rem]
-    px-4
-    text-center
-    whitespace-normal
+    hover:text-white duration-1000 rounded-[1.25rem]
+    py-2 sm:py-3
+    px-3 sm:px-4
+    text-[clamp(0.65rem,1.5vw,1rem)] tracking-tight
+    w-fit min-w-[6rem] max-w-[16rem]
   `;
 
   const allClasses = `${commonClasses} ${className || ""}`;
 
   const buttonContent = (
     <>
-      {/* Círculos animados posicionados relativos y centrados */}
-      <span className="absolute bg-yellow-300 w-[9rem] h-[9rem] rounded-full group-hover:scale-100 scale-0 -z-10 left-0 top-0 transform -translate-x-1/2 -translate-y-1/2 group-hover:duration-500 duration-700 origin-center transition-all"></span>
-      <span className="absolute bg-yellow-500 w-[9rem] h-[9rem] rounded-full group-hover:scale-100 scale-0 -z-10 left-0 top-0 transform -translate-x-1/2 -translate-y-1/2 group-hover:duration-700 duration-500 origin-center transition-all"></span>
-      <span className="absolute bg-red-600 w-[9rem] h-[9rem] rounded-full group-hover:scale-100 scale-0 -z-10 left-0 top-0 transform -translate-x-1/2 -translate-y-1/2 group-hover:duration-700 duration-500 origin-center transition-all"></span>
+      <span className="absolute bg-yellow-300 w-44 h-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span>
+      <span className="absolute bg-yellow-500 w-44 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>
       {texto}
     </>
   );
