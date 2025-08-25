d5452973c3beb3301cb3ef460d3b19aa498b1185
Ivan Ameri <dark.ranked@gmail.com>
2025-08-24 21:32:20 -0300

    Vuelvo atras los cambios

--- front/src/components/LienzoTexto/ContenedorFijoCentrado.tsx
+++ front/src/components/LienzoTexto/ContenedorFijoCentrado.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export const ContenedorFijoCentrado: React.FC<Props> = ({ children }) => {
   return (
-    <div className="md:relative md:w-full md:h-auto md:flex md:justify-center md:items-center fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[150px] pointer-events-none z-50">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[80%] h-[150px] pointer-events-none z-50">
       {children}
     </div>
   );
