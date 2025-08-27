f79419a6e7921bedc051c4cf270ca32bd62c671f
Ivan Ameri <dark.ranked@gmail.com>
2025-08-27 16:17:54 -0300

    Agrego cloudinary module a cursos module

--- backend/src/modules/cursos/cursos.module.ts
+++ backend/src/modules/cursos/cursos.module.ts
@@ -1,21 +1,24 @@
-import { Module } from '@nestjs/common';
+
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CloudinaryModule } from '../../modules/cloudinary/cloudinary.module';
 import { CursosController } from '../../controllers/cursos/cursos.controller';
 import { CursosService } from '../../services/cursos/cursos.service';
 import { Curso } from '../../entidades/curso.entity';
 import { ModuloEntity } from '../../entidades/modulo.entity';
 import { Inscripcion } from '../../entidades/inscripcion.entity'; 
-import { ProgresoModule } from '../../modules/progreso/progreso.module';
-import { CloudinaryModule } from '../../modules/cloudinary/cloudinary.module';
+import { ProgresoModule } from '../progreso/progreso.module';
+import { MailModule } from '../../mail/mail.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([Curso, ModuloEntity, Inscripcion]),
-    ProgresoModule,
-    CloudinaryModule,
-  ],
-  controllers: [CursosController],
-  providers: [CursosService],
-  exports: [CursosService], 
+  imports: [
+    TypeOrmModule.forFeature([Curso, ModuloEntity, Inscripcion]),
+    ProgresoModule, 
+    MailModule,
+    CloudinaryModule, 
+  ],
+  controllers: [CursosController],
+  providers: [CursosService],
+  exports: [CursosService],
 })
 export class CursosModule {}
